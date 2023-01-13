f0b6746a842bf1a1436837e8650b3ae12293d55e
alonrubintec <alonrubintech@gmail.com>
2023-01-13 17:33:48 +0200

    Initial commit

---
+++ css.py
@@ -0,0 +1,39 @@
+
+equal = """QPushButton {
+    background-color: rgb(26, 172, 255);
+    color: rgb(255, 255, 255);
+    border-radius: 16px
+}
+QPushButton:hover {
+    background-color: rgb(17, 154, 232);
+    color: rgb(232, 232, 232);
+}
+QPushButton:pressed {
+    background-color: rgb(96, 197, 255);
+}"""
+
+regular = """QPushButton {
+    background-color: rgb(238, 240, 245);
+    color: rgb(56, 185, 255);
+    border-radius: 16px
+}
+QPushButton:hover {
+    background-color: rgb(228, 231, 237);
+}
+QPushButton:pressed {
+    background-color: rgb(248, 248, 248);
+    color: rgb(94, 204, 255);
+}"""
+
+functions = """QPushButton {
+    background-color: rgb(173, 225, 255);
+    color: rgb(56, 185, 255);
+    border-radius: 16px
+}
+QPushButton:hover {
+    background-color: rgb(153, 209, 241);
+}
+QPushButton:pressed {
+    background-color: rgb(205, 237, 255);
+    color: rgb(113, 205, 255);
+}"""
